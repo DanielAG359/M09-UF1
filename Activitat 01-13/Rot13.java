ad343b5491a048775f4508dcf13bd46196856cfd
DanielAG359 <2023_daniel.acosta@iticbcn.cat>
2024-09-18 20:59:02 +0200

    classe

---
+++ Activitat 01-13/Rot13.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class Rot13 {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private final static char[] abecedario = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private final static char[] abecedarioMayusculas = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    public static String readLine() {
+        try {
+            String line = reader.readLine();
+            if (line == null) {
+                throw new RuntimeException("S'ha cridat massa cops Entrada.readLine()");
+            }
+            return line;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static String xifraRot13(String cadena) {
+        String stringFinal = "";
+        for (int i = 0; i < cadena.length(); i++){
+            char lletra = cadena.charAt(i);
+            if (Character.isLowerCase(lletra)){
+                for (int j = 0; j < abecedario.length; j ++){
+                    if (abecedario[j] == lletra){
+                        if ((j + 13) > 27){
+                            int t = j - 27;
+                            stringFinal += abecedario[t];
+                        }
+                    }
+                }
+            }else if(Character.isUpperCase(lletra)){
+                for (int j = 0; j < abecedarioMayusculas.length; j ++){
+                    if (abecedarioMayusculas[j] == lletra){
+                        if ((j + 13) > 27){
+                            int t = j - 27;
+                            stringFinal += abecedarioMayusculas[t];
+                        }
+                    }
+                }
+            }else{
+                stringFinal += lletra;
+            }
+        }
+        return stringFinal;
+    }
+    //public static String desxifraRot13(String cadena){}
+    public static void main(String[]args){
+        String line = readLine();
+        System.out.println(xifraRot13(line));
+    }
+}
